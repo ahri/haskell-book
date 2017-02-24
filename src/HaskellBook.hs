cace8a29f7d818611f154d8e2e570cb0518efa3c
Adam Piper <adam@ahri.net>
2017-02-24 22:15:00 +0900

    Add solutions for Recursion chapter.

--- src/HaskellBook.hs
+++ src/HaskellBook.hs
@@ -1,1 +1,30 @@
 module HaskellBook where
+
+import Data.List (intersperse)
+
+mc91 :: Integer -> Integer
+mc91 n
+    | n > 100   = n - 10
+    | otherwise = mc91 . mc91 $ n + 11
+
+wordNumber :: Integer -> String
+wordNumber n = concat . intersperse "-" $ (digitToWord <$> (digits n))
+    where
+        digits :: Integer -> [Integer]
+        digits i
+            | i < 10    = [i]
+            | otherwise = (digits . (flip div) 10 $ i) ++ [mod i 10]
+
+        digitToWord :: Integer -> String
+        digitToWord i = case i of
+            0 -> "zero"
+            1 -> "one"
+            2 -> "two"
+            3 -> "three"
+            4 -> "four"
+            5 -> "five"
+            6 -> "six"
+            7 -> "seven"
+            8 -> "eight"
+            9 -> "nine"
+            _ -> undefined
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,8 +1,11 @@
 import Test.Hspec
-import HaskellBook ()
+import HaskellBook
 
 main :: IO ()
 main = hspec $ do
-    describe "Foo" $ do
-        it "bar" $ do
-            True `shouldBe` True
+    describe "Recursion" $ do
+        it "McCarthy 91 function" $ do
+            map mc91 [95..110] `shouldBe` [91,91,91,91,91,91,91,92,93,94,95,96,97,98,99,100]
+
+        it "WordNumber" $ do
+            wordNumber 12324546 `shouldBe` "one-two-three-two-four-five-four-six"
